828b98bafc5d26b35971a19cc885f936be44b32e
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2021-03-12 13:39:55 +0100

    Add storage conversion verb

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ The package provides the additional commands for the ROS 2 bag command line tool
     tests_require=['pytest'],
     entry_points={
         'ros2bag.verb': [
+            'convert = ros2bag_tools.verb.convert:ConvertVerb',
             'cut = ros2bag_tools.verb.cut:CutVerb',
             'extract = ros2bag_tools.verb.extract:ExtractVerb',
             'replace = ros2bag_tools.verb.replace:ReplaceVerb',
