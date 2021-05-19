f265f0e19545aa131a608a9542b39fe356523ee6
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2021-05-19 16:31:33 +0200

    Add write option to video verb

--- ros2bag_tools/ros2bag_tools/verb/video.py
+++ ros2bag_tools/ros2bag_tools/verb/video.py
@@ -15,6 +15,7 @@
 import os
 import cv2
 from cv_bridge import CvBridge
+from ros2bag_tools.verb import ProgressTracker
 from rosbag2_tools.bag_view import BagView
 from ros2bag.api import print_error
 from ros2bag.verb import VerbExtension
@@ -32,6 +33,66 @@ def get_screen_size():
     return (screen_height, screen_width)
 
 
+class VideoWriter:
+
+    def __init__(self, file_path, fps):
+        self._writer = cv2.VideoWriter()
+        self._file_path = file_path
+        self._fps = fps
+        self._initialized = False
+
+    def __del__(self):
+        self._writer.release()
+
+    def process(self, image):
+        if not self._initialized:
+            fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+            video_size = (image.shape[1], image.shape[0])
+            has_color = len(image.shape) >= 3 and image.shape[2] != 1
+            self._writer.open(self._file_path, fourcc,
+                              self._fps, video_size, has_color)
+            assert(self._writer.isOpened())
+            self._initialized = True
+        self._writer.write(image)
+
+
+class VideoDisplay:
+
+    def __init__(self, _window_name):
+        frame_rate = 30
+        self._wait_ms = int(1000 / frame_rate)
+        self._target_size = None
+        self._window_name = _window_name
+
+    def __del__(self):
+        cv2.destroyAllWindows()
+
+    def process(self, image):
+        if not self._target_size:
+            screen_size = get_screen_size()
+            self._target_size = image.shape
+            while screen_size[0] < self._target_size[0]:
+                self._target_size = (
+                    int(self._target_size[0] / 2), int(self._target_size[1] / 2))
+
+        image = cv2.resize(
+            image, (self._target_size[1], self._target_size[0]), interpolation=cv2.INTER_AREA)
+        cv2.imshow(self._window_name, image)
+        key_pressed = cv2.waitKey(self._wait_ms) & 0xFF
+        if key_pressed in [ord('q'), 27]:
+            return False
+        if cv2.getWindowProperty(self._window_name, 0) < 0:
+            # handle case where user closed window
+            return False
+        return True
+
+
+def get_fps(reader, topic_name):
+    metadata = reader.get_metadata()
+    for entry in metadata.topics_with_message_count:
+        if entry.topic_metadata.name == topic_name:
+            return entry.message_count / metadata.duration.total_seconds()
+
 
 class VideoVerb(VerbExtension):
     """Display or write a video of image data in a bag."""
@@ -46,8 +107,13 @@ class VideoVerb(VerbExtension):
             '-f', '--serialization-format', default='',
             help='rmw serialization format in which the messages are read, defaults to the'
                  ' rmw currently in use')
+        parser.add_argument(
+            '--progress', default=False, action='store_true',
+            help='display reader progress in terminal')
         parser.add_argument('-t', '--topic', required=True, type=str,
                             help='image topic to read')
+        parser.add_argument('-o', '--output', type=str,
+                            help='file path to video to write')
 
     def main(self, *, args):  # noqa: D102
         if not os.path.exists(args.bag_file):
@@ -64,7 +130,6 @@ class VideoVerb(VerbExtension):
             ConverterOptions,
             StorageFilter
         )
-        screen_size = get_screen_size()
 
         reader = SequentialReader()
         in_storage_options = StorageOptions(
@@ -77,29 +142,23 @@ class VideoVerb(VerbExtension):
         filter = StorageFilter()
         filter.topics = [args.topic]
 
+        progress = ProgressTracker()
+        if args.progress:
+            progress.add_estimated_work(reader, filter)
+
         # TODO ensure the input topic is an image topic
         bag_view = BagView(reader, filter)
-        frame_rate = 30
-        wait_ms = int(1000 / frame_rate)
+        if args.output:
+            processor = VideoWriter(args.output, get_fps(reader, args.topic))
+        else:
+            processor = VideoDisplay(args.topic)
 
-        target_size = None
-
-        window_name = args.topic
+        image_bridge = CvBridge()
         for _, image, _ in bag_view:
-            if not target_size:
-                target_size = (image.height, image.width)
-                while screen_size[0] < target_size[0]:
-                    target_size = (int(target_size[0] / 2), int(target_size[1] / 2))
-            image_bridge = CvBridge()
             cv_image = image_bridge.imgmsg_to_cv2(image)
-            cv_image = cv2.resize(cv_image, (target_size[1], target_size[0]), interpolation=cv2.INTER_AREA)
-            cv2.imshow(window_name, cv_image)
-            key_pressed = cv2.waitKey(wait_ms) & 0xFF
-            if key_pressed in [ord('q'), 27]:
-                break
-            if cv2.getWindowProperty(window_name, 0) < 0:
-                # handle case where user closed window
-                break
-        
-        cv2.destroyAllWindows()
+            processor.process(cv_image)
+            if args.progress:
+                progress.print_update(progress.update(args.topic), every=10)
 
+        if args.progress:
+            progress.print_finish()
