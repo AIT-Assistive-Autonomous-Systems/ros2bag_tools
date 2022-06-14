d55616022c3f01224f1d2dd68bbe7173a4fdd5ca
Fabio Oberweger <fabio.oberweger@ait.ac.at>
2022-06-14 10:54:06 +0000

    Add flag to modify msg header with offset

--- ros2bag_tools/ros2bag_tools/filter/restamp.py
+++ ros2bag_tools/ros2bag_tools/filter/restamp.py
@@ -12,11 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from rclpy.time import Time
-from rclpy.serialization import deserialize_message
+import argparse
+from rclpy.time import Time, Duration
+from rclpy.serialization import deserialize_message, serialize_message
 from rosidl_runtime_py.utilities import get_message
 from ros2bag_tools.filter import FilterExtension
 import re
+
+def nanoseconds_duration(data: str):
+    try:
+        val = int(data)
+    except:
+        val = int(float(data) * 10**9)
+    return Duration(nanoseconds=val)
+
 
 class RestampFilter(FilterExtension):
 
@@ -27,11 +36,14 @@ class RestampFilter(FilterExtension):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '-u', '--offset-topic', nargs='+',
+            '-u', '--offset-topic', nargs='+', default=[],
             help='topics to restamp with offset as regex string')
         parser.add_argument(
-            '-c', '--offset', default=0, type=int,
-            help='constant offset value in seconds applied to offset topics')
+            '-c', '--offset', default='0', type=nanoseconds_duration,
+            help='constant offset value in seconds (float) or nanoseconds (int) applied to offset topics')
+        parser.add_argument(
+            '-m', '--modify-msg-header', action='store_true',
+            help='apply offset to header if offset is enabled')
 
     def set_args(self, metadatas, args):
         self._offset_topics = set()
@@ -40,7 +52,8 @@ class RestampFilter(FilterExtension):
                 topic_name = topic.topic_metadata.name
                 if any([re.match(r, topic_name) for r in args.offset_topic]):
                     self._offset_topics.add(topic_name)
-        self._offset = args.offset * (10 ** 9)
+        self._offset = args.offset
+        self._modify_header = args.modify_msg_header
 
     def filter_topic(self, topic_metadata):
         topic_type = topic_metadata.type
@@ -56,7 +69,12 @@ class RestampFilter(FilterExtension):
         (topic, data, t) = serialized_msg
         msg = deserialize_message(data, self._topic_type_map[topic])
         if hasattr(msg, 'header'):
-            t = Time.from_msg(msg.header.stamp).nanoseconds
+            header_time = Time.from_msg(msg.header.stamp)
+            t = header_time.nanoseconds
         if topic in self._offset_topics:
-            t = t + self._offset
+            t = t + self._offset.nanoseconds
+            if hasattr(msg, 'header') and self._modify_header:
+                header_time += self._offset
+                msg.header.stamp = header_time.to_msg()
+                data = serialize_message(msg)
         return (topic, data, t)
