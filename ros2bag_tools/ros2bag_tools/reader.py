360b5d54b10608ee04e2a3f78a69ebea660ac18f
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2022-11-25 15:03:58 +0100

    Refactor filtered reader

--- ros2bag_tools/ros2bag_tools/reader.py
+++ ros2bag_tools/ros2bag_tools/reader.py
@@ -14,7 +14,20 @@
 
 from rosidl_runtime_py.utilities import get_message
 from rclpy.serialization import deserialize_message
-from rosbag2_py import TopicMetadata
+from rosbag2_tools import default_rosbag_options
+from ros2bag_tools.filter import FilterExtension, FilterResult
+from rosbag2_py import TopicMetadata, StorageFilter, SequentialReader
+
+ReadOrder = None
+ReadOrderSortBy = None
+
+try:
+    from rosbag2_py import (
+        ReadOrder,
+        ReadOrderSortBy
+    )
+except:
+    pass
 
 
 class TopicDeserializer:
@@ -35,3 +48,70 @@ class TopicDeserializer:
     def deserialize(self, topic, data):
         return deserialize_message(data, self._topic_type_map[topic])
 
+
+class FilteredReader:
+
+    def __init__(self, bag_paths, filter: FilterExtension, in_storage='', topics=None):
+        assert(len(bag_paths) > 0)
+
+        storage_filter = filter.get_storage_filter()
+        if topics:
+            if not storage_filter:
+                storage_filter = StorageFilter(topics)
+            else:
+                topics = list(set(topics).union(set(storage_filter.topics)))
+                storage_filter = StorageFilter(topics)
+
+        self._readers = []
+        for bag_path in bag_paths:
+            reader = SequentialReader()
+            if ReadOrder:
+                reader.set_read_order(ReadOrder(ReadOrderSortBy.ReceivedTimestamp))
+            in_s_opts, in_c_opts = default_rosbag_options(bag_path, in_storage)
+            reader.open(in_s_opts, in_c_opts)
+            if storage_filter:
+                reader.set_filter(storage_filter)
+            self._readers.append(reader)
+        self._filter = filter
+        self._queue = []
+
+    def get_all_topics_and_types(self):
+        for reader in self._readers:
+            for topic_metadata in reader.get_all_topics_and_types():
+                result = self._filter.filter_topic(topic_metadata)
+                if result:
+                    if isinstance(result, list):
+                        for item in result:
+                            yield item
+                    else:
+                        yield result
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self._queue) > 0:
+            return self._queue.pop(0)
+        while True:
+            if len(self._readers) == 0:
+                raise StopIteration()
+            if not self._readers[0].has_next():
+                self._readers.pop(0)
+                continue  # check reader again
+
+            msg = self._readers[0].read_next()
+            result = self._filter.filter_msg(msg)
+            if result == FilterResult.STOP_CURRENT_BAG:
+                raise StopIteration()
+            elif result == FilterResult.DROP_MESSAGE:
+                continue
+            elif isinstance(result, list):
+                if len(result) == 0:
+                    continue
+                self._queue = result[1:]
+                return result[0]
+            elif isinstance(result, tuple):
+                return result
+            else:
+                raise ValueError(
+                    "Filter returned invalid result: '{}'.".format(result))
--- ros2bag_tools/ros2bag_tools/verb/__init__.py
+++ ros2bag_tools/ros2bag_tools/verb/__init__.py
@@ -17,7 +17,6 @@ import os
 from datetime import datetime
 from rosbag2_py import (
     Info,
-    SequentialReader,
     SequentialWriter,
     StorageOptions,
     ConverterOptions,
@@ -26,28 +25,20 @@ from rosbag2_py import (
 )
 from ros2bag_tools.progress import ProgressTracker
 
-ReadOrder = None
-ReadOrderSortBy = None
-
-try:
-    from rosbag2_py import (
-        ReadOrder,
-        ReadOrderSortBy
-    )
-except:
-    pass
-
+from ros2bag_tools.reader import FilteredReader
 from ros2bag.api import print_error
 from ros2bag.verb import VerbExtension
-from ros2bag_tools.filter import FilterResult
+
 
 def get_rosbag_options(args):
     """Get rosbag options from args matching the ros2bag.api.add_standard_reader_args names"""
-    storage_id = args.storage if hasattr(args,'storage') else 'sqlite3'
+    storage_id = args.storage if hasattr(args, 'storage') else 'sqlite3'
     serialization_format = (
-        args.serialization_format if hasattr(args,'serialization_format') else 'cdr'
+        args.serialization_format if hasattr(
+            args, 'serialization_format') else 'cdr'
     )
-    max_bagfile_size = args.max_bag_size if hasattr(args, 'max_bag_size') else 0
+    max_bagfile_size = args.max_bag_size if hasattr(
+        args, 'max_bag_size') else 0
     storage_options = StorageOptions(
         uri=args.bag_path,
         storage_id=storage_id,
@@ -73,7 +64,7 @@ class FilterVerb(VerbExtension):
             defaults to a timestamped folder in the current directory')
         parser.add_argument(
             '-s', '--in-storage', default='sqlite3',
-            choices = get_registered_readers(),
+            choices=get_registered_readers(),
             help='storage identifier to be used for the input bag, defaults to "sqlite3"')
         parser.add_argument(
             '-b', '--max-bag-size', type=int, default=0,
@@ -83,15 +74,14 @@ class FilterVerb(VerbExtension):
         )
         parser.add_argument(
             '--out-storage', default='sqlite3',
-            choices = get_registered_writers(),
+            choices=get_registered_writers(),
             help='storage identifier to be used for the output bag, defaults to "sqlite3"')
         parser.add_argument(
             '-f', '--serialization-format', default='cdr',
             help='rmw serialization format in which the messages are saved, defaults to the'
                  ' rmw currently in use')
-        parser.add_argument(
-            '--progress', action='store_true',
-            help='show progress bar')
+        parser.add_argument('--progress', action='store_true',
+                            help='show progress bar')
         self._filter.add_arguments(parser)
 
     def main(self, *, args):  # noqa: D102
@@ -105,69 +95,40 @@ class FilterVerb(VerbExtension):
             return print_error("Output folder '{}' already exists.".format(uri))
 
         info = Info()
-        metadatas = [info.read_metadata(f, args.in_storage or '') for f in args.bag_files]
+        metadatas = [info.read_metadata(
+            f, args.in_storage or '') for f in args.bag_files]
         try:
             self._filter.set_args(metadatas, args)
         except argparse.ArgumentError as e:
             return print_error(str(e))
 
-        storage_filter = self._filter.get_storage_filter()
+        progress = None
+        if args.progress:
+            progress = ProgressTracker()
 
-        args_in_bag = vars(args).copy()
-        args_in_bag.pop('bag_files')
-        args_out_bag = args_in_bag.copy()
-        args_in_bag['storage'] = args.in_storage
-        args_in_bag['max_bag_size'] = 0
-        args_out_bag['storage'] = args.out_storage
-        args_out_bag['bag_path'] = uri
+        reader = FilteredReader(args.bag_files, self._filter, args.in_storage)
 
-        progress = ProgressTracker()
-        readers = []
-        for bag_file, metadata in zip(args.bag_files, metadatas):
-            reader = SequentialReader()
-            args_in_bag['bag_path'] = bag_file
-            if ReadOrder:
-                reader.set_read_order(ReadOrder(ReadOrderSortBy.ReceivedTimestamp))
-            in_storage_options, in_converter_options = get_rosbag_options(
-                argparse.Namespace(**args_in_bag))
-            reader.open(in_storage_options, in_converter_options)
-            if storage_filter:
-                reader.set_filter(storage_filter)
-            if args.progress:
-                progress.add_estimated_work(metadata, self._filter.output_size_factor(metadata))
-            readers.append(reader)
+        if progress:
+            for metadata in metadatas:
+                progress.add_estimated_work(
+                    metadata, self._filter.output_size_factor(metadata))
 
         writer = SequentialWriter()
+        args_out_bag = vars(args).copy()
+        args_out_bag['storage'] = args.out_storage
+        args_out_bag['bag_path'] = uri
         out_storage_options, out_converter_options = get_rosbag_options(
             argparse.Namespace(**args_out_bag))
         writer.open(out_storage_options, out_converter_options)
 
-        for reader in readers:
-            for topic_metadata in reader.get_all_topics_and_types():
-                result = self._filter.filter_topic(topic_metadata)
-                if result:
-                    if not isinstance(result, list):
-                        result = [result]
-                    for item in result:
-                        writer.create_topic(item)
+        for topic in reader.get_all_topics_and_types():
+            writer.create_topic(topic)
 
-        for reader in readers:
-            while reader.has_next():
-                msg = reader.read_next()
-                result = self._filter.filter_msg(msg)
-                if args.progress:
-                    prog_perc = progress.update(msg[0])
-                    progress.print_update(prog_perc)
-                if result == FilterResult.STOP_CURRENT_BAG:
-                    break
-                elif result == FilterResult.DROP_MESSAGE:
-                    continue
-                elif isinstance(result, list):
-                    for item in result:
-                        writer.write(item[0], item[1], item[2])
-                elif isinstance(result, tuple):
-                    writer.write(result[0], result[1], result[2])
-                else:
-                    return print_error("Filter returned invalid result: '{}'.".format(result))
-        if args.progress:
+        for item in reader:
+            if progress:
+                prog_perc = progress.update(item[0])
+                progress.print_update(prog_perc)
+            writer.write(*item)
+
+        if progress:
             progress.print_finish()
