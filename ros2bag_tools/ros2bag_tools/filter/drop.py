02ff752f4da03ad34aa04f4f0f2476c8afd083c4
Björn Wachtendorf <b.wachtendorf@cellumation.com>
2025-03-27 13:30:03 +0100

    Drop messages by ratio instead of counter

--- ros2bag_tools/ros2bag_tools/filter/drop.py
+++ ros2bag_tools/ros2bag_tools/filter/drop.py
@@ -14,8 +14,8 @@
 
 from typing import Dict, Sequence
 
-from ros2bag_tools.filter import FilterExtension
-from ros2bag_tools.filter import FilterResult
+from ros2bag.api import print_error
+from ros2bag_tools.filter import FilterExtension, FilterResult
 
 
 class DropFilter(FilterExtension):
@@ -24,9 +24,8 @@ class DropFilter(FilterExtension):
     def __init__(self):
         super().__init__()
         self._topics: Sequence[str] = []
-        self._x = 0
-        self._y = 0
-        self._msg_counters: Dict[str, int] = {}
+        self._ratio: float = 1.0
+        self._msg_counters: Dict[str, Dict[str, int]] = {}
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -39,17 +38,32 @@ class DropFilter(FilterExtension):
             ),
         )
         parser.add_argument(
-            '-x', type=int, required=True, help='count of messages out of ytodrop'
+            "-x",
+            type=int,
+            required=True,
+            help="Count of messages out of y to drop. Must be > 0.",
         )
         parser.add_argument(
-            '-y', type=int, required=True, help='module of the message counter'
+            "-y",
+            type=int,
+            required=True,
+            help="Module of the message counter. Must be > 0 and => x.",
         )
 
     def set_args(self, _metadata, args):
+        if not args.y > 0:
+            print(print_error("y must be > 0"))
+            exit(1)
+        if not args.x > 0:
+            print(print_error("x must be > 0"))
+            exit(1)
+        if not args.y >= args.x:
+            print(print_error("y must be => x"))
+            exit(1)
+
         self._topics = args.topics
-        self._x = args.x
-        self._y = args.y
-        assert self._x <= self._y
+        self._ratio = args.x / args.y
+        self._sample_size = args.y
 
     def _is_drop_topic(self, topic: str) -> bool:
         """Return a boolean indicating whether we're interested in filtering this topic."""
@@ -62,17 +76,25 @@ class DropFilter(FilterExtension):
         (topic, _, _) = msg
 
         if self._is_drop_topic(topic):
-            do_drop = False
-
+            # initialize counters
             if topic not in self._msg_counters:
-                self._msg_counters[topic] = 0
+                self._msg_counters[topic] = {"total": 0, "dropped": 0}
 
-            if self._msg_counters[topic] >= self._y:
-                self._msg_counters[topic] = 0
-            if self._msg_counters[topic] < self._x:
-                do_drop = True
-            self._msg_counters[topic] += 1
-            if do_drop:
+            # reset counters if we have reached sample size
+            if self._msg_counters[topic]["total"] == self._sample_size:
+                self._msg_counters[topic] = {"total": 0, "dropped": 0}
+
+            self._msg_counters[topic]["total"] += 1
+
+            # calculate current dropped messages ratio
+            ratio = (
+                self._msg_counters[topic]["dropped"]
+                / self._msg_counters[topic]["total"]
+            )
+
+            # drop message if we are currently below target ratio
+            if ratio < self._ratio:
+                self._msg_counters[topic]["dropped"] += 1
                 return FilterResult.DROP_MESSAGE
 
         return msg
